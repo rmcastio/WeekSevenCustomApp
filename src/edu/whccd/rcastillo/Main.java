cbd6bc8d1272f517d4fcbf37943f7a5a4b5f5d32
RichardCastillo <armed_deity@hotmail.com>
2020-05-08 20:13:21 -0700

    Initial commit

---
+++ src/edu/whccd/rcastillo/Main.java
@@ -0,0 +1,23 @@
+package edu.whccd.rcastillo;
+
+ /* This Custom Application is to display the use of Arrays and ArrayLists.
+    Author: Richard Castillo
+  */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] grades = {0, 1, 2, 3, 4};
+        grades[0];
+        grades[1];
+        grades[2];
+        grades[3];
+        grades[4];
+    }
+}
